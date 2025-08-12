f761e7d475e17b64314793f29243de27b2854426
Ankit-1204 <ankit2003upadhyay@gmail.com>
2025-08-13 04:04:00 +0530

    add models

---
+++ backend/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker,declarative_base
+import os
+
+DATABASE_URL=os.getenv(
+    "DATABASE_URL",
+    "postgresql://myuser:mypassword@db:5432/workflow_db"
+)
+
+engine=create_engine(
+    DATABASE_URL
+)
+
+# used to interact with DB 
+session=sessionmaker(autocommit=False,autoflush=False,bind=engine)
+
+# this is the base class for all db model
+Base=declarative_base()
---
+++ backend/models.py
@@ -0,0 +1,25 @@
+from .database import Base
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+
+class Workflow(Base):
+    __tablename__='workflows'
+
+    id=Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    definition = Column(Text, nullable=False)  
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    # relationships, to connect instances of tables already connected with foregin keys
+    # behaviour changes depending on where foreign key is. 1-to-many vs 1-to-1
+    instances = relationship("WorkflowInstance", back_populates="workflow")
+
+class WorkflowRun(Base):
+    __tablename__="workflow_runs"
+    id = Column(Integer, primary_key=True, index=True)
+    workflow_id = Column(Integer, ForeignKey("workflows.id"), nullable=False)
+    status = Column(String(50), default="pending", index=True)
+    started_at = Column(DateTime(timezone=True), server_default=func.now())
+    completed_at = Column(DateTime(timezone=True))
+    workflow=relationship("Workflow", back_populates="instances")
